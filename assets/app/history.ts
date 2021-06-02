0f594e4ec8f7c3e6f210d7bf84f2de64f315d62e
GauBen <benaim.gautier@gmail.com>
2021-06-02 22:19:12 +0200

    fix: axes direction

--- assets/app/history.ts
+++ assets/app/history.ts
@@ -25,15 +25,15 @@ const main = async () => {
       <svg class="image" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="48" height="48" viewBox="-1 -1 2 2" xml:space="preserve" style="stroke-width:.09;stroke-linecap:round">
         <line style="stroke:#000;" x1="-.95" y1="0" x2=".95" y2="0"/>
         <line style="stroke:#000" x1="0" y1="-.95" x2="0" y2=".95"/>
-        <line style="stroke:#f00" x1="${first.x}" y1="${first.y}" x2="${
+        <line style="stroke:#f00" x1="${first.x}" y1="${-first.y}" x2="${
       last.x
-    }" y2="${last.y}"/>
-        <circle style="stroke:#f00;fill:#fff" cx="${first.x}" cy="${
-      first.y
-    }" r=".1"/>
-        <circle style="stroke:#f00;fill:#fff" cx="${last.x}" cy="${
-      last.y
-    }" r=".1"/>
+    }" y2="${-last.y}"/>
+        <circle style="stroke:#f00;fill:#fff" cx="${
+          first.x
+        }" cy="${-first.y}" r=".1"/>
+        <circle style="stroke:#f00;fill:#fff" cx="${
+          last.x
+        }" cy="${-last.y}" r=".1"/>
       </svg>
       <p class="title">From <strong>${escapeHtml(
         first.title
